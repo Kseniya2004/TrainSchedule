857aa3f243d9c8a0edc1ab5643fc9ecbe8e0e6b6
Немтырёва Ксения <k.nemtyryova@gmail.com>
2022-11-29 14:55:42 +0300

    Добавьте файлы проекта.

---
+++ Pages/PageTrain.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrainSchedule.Classes;
+
+namespace TrainSchedule.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageTrain.xaml
+    /// </summary>
+    public partial class PageTrain : Page
+    {
+        public PageTrain()
+        {
+            InitializeComponent();
+            //привязка таблицы            
+            DgridTrain.ItemsSource = Train_scheduleEntities.GetTrain().Train.ToList();
+            
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Train_scheduleEntities.GetTrain().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DgridTrain.ItemsSource = Train_scheduleEntities.GetTrain().Train.ToList();
+            }
+        }
+    }
+}
