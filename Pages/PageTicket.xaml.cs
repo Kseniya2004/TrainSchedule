5c1492d0d567da5114191fad3973e20c72615742
Kseniya2004 <98638790+Kseniya2004@users.noreply.github.com>
2022-12-05 23:05:58 +0300

    Add files via upload

--- Pages/PageTicket.xaml.cs
+++ Pages/PageTicket.xaml.cs
@@ -33,28 +33,37 @@ namespace TrainSchedule.Pages
         private void BtnExcel_Click(object sender, RoutedEventArgs e)
         {
             Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook wb = excelApp.Workbooks.Open($"{Directory.GetCurrentDirectory()}\\Шаблон.xlsx");
+            Excel.Workbook wb = excelApp.Workbooks.Open($"{Directory.GetCurrentDirectory()}\\Отчёт по продажам.xlsx");
             Excel.Worksheet ws = (Excel.Worksheet)wb.Worksheets[1];
-            ws.Cells[4, 2] = DateTime.Now.ToString();            
+            ws.Cells[1, 6] = DateTime.Now.ToString();            
             int indexRows = 1;
+            
             //ячейка
-            ws.Cells[1][indexRows] = "Номер";
-            ws.Cells[2][indexRows] = "Цена";
-            ws.Cells[3][indexRows] = "Номер поезда";
-            ws.Cells[4][indexRows] = "ФИО";    
+            ws.Cells[1][indexRows] = "Номер";            
+            ws.Cells[2][indexRows] = "Номер поезда";
+            ws.Cells[3][indexRows] = "ФИО";    
+            ws.Cells[4][indexRows] = "Цена";
 
             //список пользователей из таблицы после фильтрации и поиска
             var printItems = DgridTicket.Items;
             //цикл по данным из списка для печати
             foreach (Ticket item in printItems)
             {
-                ws.Cells[1][indexRows + 1] = indexRows;
-                ws.Cells[2][indexRows + 1] = item.price;
-                ws.Cells[3][indexRows + 1] = item.Train.num.ToString();
-                ws.Cells[4][indexRows + 1] = item.Pessenger.fio.ToString();                
+                ws.Cells[1][indexRows + 1] = indexRows;   
+                ws.Cells[2][indexRows + 1] = item.Train.num.ToString();                
+                ws.Cells[3][indexRows + 1] = item.Pessenger.fio;                
+                ws.Cells[4][indexRows + 1] = item.price;
+                
 
                 indexRows++;
             }
+            Excel.Range range = ws.Range[ws.Cells[2][indexRows + 1], ws.Cells[5][indexRows + 1]];
+            range.ColumnWidth = 15; //ширина столбцов
+            range.Font.Italic = true;
+            range.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;//выравнивание по левому краю
+
+            ws.Cells[3, 6] = "Итого:";
+            ws.Cells[3, 7].Formula = $"=SUM(D:D)";
             ws.Cells[indexRows + 2, 3] = "Подпись";
             ws.Cells[indexRows + 2, 5] = "Немтырёва К.А.";
             excelApp.Visible = true;
